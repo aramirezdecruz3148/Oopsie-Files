33a449e4904da43f44bcab333f2df5a30afbca97
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-26 19:59:32 -0700

    completed next step to change a single file name. Created a test and a new corresponding function called rename, in test passed a specific file name and a new name. Test is passing

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Helping a fictional friend to rename a large batch of files
   1. ~~get random animal species~~
   1. ~~write a bunch of files with animal species as content~~
 1. ~~get all files in target directory~~
-1. rename a file
+1. ~~rename a file~~
 1. get last modified date of a file
 1. get files content
 1. rename all files as `content-oldFileNumber-date`
--- rename-files.test.js
+++ rename-files.test.js
@@ -1,14 +1,43 @@
+const fs = require('fs');
 const { createFiles } = require('./create-files');
-const { readDirectory } = require('./rename-files');
+const { readDirectory, rename  } = require('./rename-files');
 
 describe('rename files', () => {
   beforeEach(done => {
     createFiles('./fixtures', 100, done);
   });
+
+  afterEach(done => {
+    fs.readdir('./fixtures', (err, files) => {
+      if(files.length === 0) done();
+      let deletedSoFar = 0;
+      files.forEach(file => {
+        fs.unlink(`./fixtures/${file}`, err => {
+          if(err) return done(err);
+          deletedSoFar += 1;
+          if(deletedSoFar === files.length) done();
+        });
+      });
+    });
+  });
+
   it('gets all files in fixtures directory', done => {
     readDirectory('./fixtures', (err, files) => {
       expect(files).toHaveLength(100);
       done();
+    });
+  });
+
+  it('can rename a file, passing in a path and new path', done => {
+    fs.readFile('./fixtures/0.txt', { encoding: 'utf8' }, (err, oldFileContent) => {
+      rename('./fixtures/0.txt', './fixtures/new-name.txt', err => {
+        expect(err).toBeFalsy();
+        fs.readFile('./fixtures/new-name.txt', { encoding: 'utf8' }, (err, newFileContent) => {
+          expect(err).toBeFalsy();
+          expect(newFileContent).toEqual(oldFileContent);
+          done();
+        });
+      });
     });
   });
 });
